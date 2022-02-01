f7c5bb0c0fe686cde609d097508425b3749c7700
Jeremy Winterberg <jeremy@jeremydwayne.com>
2022-01-31 20:07:45 -0600

    DataGrip Data Extractor: generates where in clause

---
+++ SQL-WHERE-IN.groovy
@@ -0,0 +1,29 @@
+SEP = ", "
+QUOTE     = "\'"
+NEWLINE   = System.getProperty("line.separator")
+
+begin = true
+
+def record(columns, dataRow) {
+
+    if (begin) {
+        OUT.append("WHERE ")
+        columns.eachWithIndex { column, idx ->
+            OUT.append(column.name()).append(" IN (").append(NEWLINE)
+        }
+        begin = false
+    }
+    else {
+        OUT.append(",").append(NEWLINE)
+    }
+
+    columns.eachWithIndex { column, idx ->
+        def skipQuote = dataRow.value(column).toString().isNumber() || dataRow.value(column) == null
+        def stringValue = FORMATTER.format(dataRow, column)
+        OUT.append(skipQuote ? "": QUOTE).append(stringValue.replace(QUOTE, QUOTE + QUOTE))
+           .append(skipQuote ? "": QUOTE).append(idx != columns.size() - 1 ? SEP : "")
+    }
+}
+
+ROWS.each { row -> record(COLUMNS, row) }
+OUT.append(")")
